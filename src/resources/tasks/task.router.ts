14d60b20c9d8a980c64276370a2974d0f8a5322c
Eugene Zhuchenko <jekachenko@gmail.com>
2021-06-20 15:33:19 +0300

    Refactor routers

--- src/resources/tasks/task.router.ts
+++ src/resources/tasks/task.router.ts
@@ -4,48 +4,55 @@ import Task from './task.model';
 import tasksService from './task.service';
 
 const router = Router({ mergeParams: true });
+const { OK, CREATED, NOT_FOUND, NO_CONTENT } = StatusCodes;
 
-router.route('/').get(async (_: Request, res: Response) => {
-  const tasks = await tasksService.getAll();
-  res.status(StatusCodes.OK).json(tasks.map(Task.toResponse));
-});
+router
+  .route('/')
 
-router.route('/:id').get(async ({ params }: Request, res: Response) => {
-  const { id = '' } = params;
-  const task = await tasksService.get(id);
-  if (task) {
-    res.status(StatusCodes.OK).json(Task.toResponse(task));
-  } else {
-    res.status(StatusCodes.NOT_FOUND).json('Not found');
-  }
-});
+  .get(async (_: Request, res: Response) => {
+    const tasks = await tasksService.getAll();
+    res.status(OK).json(tasks.map(Task.toResponse));
+  })
 
-router.route('/').post(async ({ body, params }: Request, res: Response) => {
-  const { boardId } = params;
-  const task = await tasksService.create(
-    Task.fromRequest({ ...body, boardId }),
-  );
+  .post(async ({ body, params }: Request, res: Response) => {
+    const { boardId } = params;
+    const task = await tasksService.create(
+      Task.fromRequest({ ...body, boardId }),
+    );
 
-  if (task) {
-    res.status(StatusCodes.CREATED).json(Task.toResponse(task));
-  } else {
-    res.status(StatusCodes.NOT_FOUND).json('Not found');
-  }
-});
+    if (task) {
+      res.status(CREATED).json(Task.toResponse(task));
+    } else {
+      res.status(NOT_FOUND).json('Not found');
+    }
+  });
 
-router.route('/:id').delete(async ({ params }: Request, res: Response) => {
-  const { id = '' } = params;
-  await tasksService.remove(id);
+router
+  .route('/:id')
 
-  res.status(StatusCodes.OK).json('ok');
-});
+  .get(async ({ params }: Request, res: Response) => {
+    const { id = '' } = params;
+    const task = await tasksService.get(id);
+    if (task) {
+      res.status(OK).json(Task.toResponse(task));
+    } else {
+      res.status(NOT_FOUND).json('Not found');
+    }
+  })
 
-router.route('/:id').put(async ({ params, body }: Request, res: Response) => {
-  const { id } = params;
-  const task = Task.fromRequest({ ...body, id });
-  await tasksService.update(task);
+  .put(async ({ params, body }: Request, res: Response) => {
+    const { id } = params;
+    const task = Task.fromRequest({ ...body, id });
+    await tasksService.update(task);
 
-  res.status(StatusCodes.OK).json(task);
-});
+    res.status(OK).json(task);
+  })
+
+  .delete(async ({ params }: Request, res: Response) => {
+    const { id = '' } = params;
+    await tasksService.remove(id);
+
+    res.status(NO_CONTENT).send({ message: 'DELETED' });
+  });
 
 export default router;
