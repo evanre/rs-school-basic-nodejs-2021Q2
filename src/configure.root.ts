06b50736b879520091a90b6c32eb6a1f4b61e1c7
Eugene Zhuchenko <jekachenko@gmail.com>
2021-07-10 12:40:37 +0300

    Add migrations, fix ormConfig

--- src/configure.root.ts
+++ src/configure.root.ts
@@ -19,19 +19,18 @@ const {
 // todo: replace POSTGRES_HOST when setup docker for nodejs
 const POSTGRES_HOST = 'localhost';
 
-export const ormConfig = {
+export default {
   type: 'postgres',
   host: POSTGRES_HOST, // postgres container name
   port: Number(POSTGRES_PORT),
   username: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
   database: POSTGRES_DB,
-  entities: [User, Board],
-  // migrations: ['src/migrations/*.ts'],
-  // synchronize: false,
-  synchronize: true,
-  // migrationsRun: true,
-  // cli: {
-  //   migrationsDir: 'src/migrations',
-  // },
+  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+  migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
+  synchronize: false,
+  migrationsRun: true,
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
 } as TypeOrmModuleOptions;
